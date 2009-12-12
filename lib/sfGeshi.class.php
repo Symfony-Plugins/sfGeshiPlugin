8cdbcedf652902ff9f432bf2ce19c2c23f31d0ea
tkoomzaaskz <tkoomzaaskz@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-12-12 10:23:07 -0600

    preparing for 1.0.0 release
    git-svn-id: http://svn.symfony-project.com/plugins/sfGeshiPlugin/trunk@25287 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/sfGeshi.class.php
+++ lib/sfGeshi.class.php
@@ -1,6 +1,14 @@
 <?php
-
-
+/**
+ * sfGeshi class.
+ *
+ * Class providing easy use of GeSHi in symfony.
+ *
+ * @package    plugin
+ * @subpackage sfGeshiPlugin
+ * @class      sfGeshi
+ * @author     Tomasz Ducin <tomasz.ducin@mellerist.com>
+ */
 class sfGeshi extends GeSHi {
 
 }
