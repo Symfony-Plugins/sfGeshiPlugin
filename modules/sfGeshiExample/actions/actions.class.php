cb4b4eda4e8261e02898568b7d559cd66f2715e0
tkoomzaaskz <tkoomzaaskz@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-12-13 12:37:23 -0600

    preparing for 1.0.1 release
    git-svn-id: http://svn.symfony-project.com/plugins/sfGeshiPlugin/trunk@25319 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfGeshiExample/actions/actions.class.php
+++ modules/sfGeshiExample/actions/actions.class.php
@@ -16,7 +16,17 @@ class sfGeshiExampleActions extends sfActions
      */
     public function executeIndex(sfWebRequest $request)
     {
-        $geshi = new sfGeshi(implode("", file(__FILE__)), 'php');
-        $this->code = $geshi->parse_code();
+        $this->code_single = sfGeshi::parse_single(implode("", file(__FILE__)), 'php');
+        $this->code_mixed = sfGeshi::parse_mixed("Hello, mom, this is <b>PHP</b> language!
+[php]\n<?php\n    echo \"hello GeSHi\";\n?>\n[/]
+Wait, mom, the following is <b>C</b> language!
+[cpp]
+class Symfony {
+  public:
+    Symfony() {
+    }
+};
+[/]
+Bye, mom!");
     }
 }
--- modules/sfGeshiExample/templates/indexSuccess.php
+++ modules/sfGeshiExample/templates/indexSuccess.php
@@ -1,5 +1,10 @@
-<h1>GeSHi test</h1>
+<h1>GeSHi test 1 - parse_single</h1>
+
+<?php echo $code_single ?>
 
 <hr />
 
-<?php echo $code ?>
+<h1>GeSHi test 2 - parse_mixed</h1>
+
+<?php echo $code_mixed ?>
+
